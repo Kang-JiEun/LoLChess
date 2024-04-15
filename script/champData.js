27e318bfcefbd4b6cb51e1439e43aaf021f053be
Kang-JiEun <rkdwldms9058@naver.com>
2024-04-15 16:54:21 +0900

    시너지 이미지들 추가, champData.js에 있는 데이터값 batchTool.html의 시너지탭 부분에 나오도록 구현, 모든 챔피언 배치툴에 드래그 구현되도록 진행중

--- script/champData.js
+++ script/champData.js
@@ -1,122 +1,123 @@
-function champData(){
-    var champInformation = [
-        {
-            name : '이즈리얼',
-            synergy : '마법사',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '질리언',
-            synergy : '범죄조직',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 2
-        },{
-            name : '타릭',
-            synergy : '마법사',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '하이머딩거',
-            synergy : '요들',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '세라핀',
-            synergy : '범죄조직',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '오리아나',
-            synergy : '범죄조직',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '잔나',
-            synergy : '범죄조직',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '제이스',
-            synergy : '범죄조직',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '다리우스',
-            synergy : '범죄조직',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '자이라',
-            synergy : '범죄조직',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '탈론',
-            synergy : '암살자',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '샤코',
-            synergy : '범죄조직',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '에코',
-            synergy : '범죄조직',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '브라움',
-            synergy : '범죄조직',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '아칼리',
-            synergy : '범죄조직',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '직스',
-            synergy : '범죄조직',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '스웨인',
-            synergy : '마법사',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '말자하',
-            synergy : '마법사',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '벡스',
-            synergy : '마법사',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '럭스',
-            synergy : '마법사',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '빅토르',
-            synergy : '마법사',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },{
-            name : '유미',
-            synergy : '고냥이',
-            synergyIcon : './img/synergy/Syndicate%20(1).svg',
-            coin : 1
-        },
-    ];
-}
+
+var champInformation = [
+    {
+        name: '이즈리얼',
+        synergy: '마법사',
+        synergyIcon: './img/synergy/Scholar_white.svg',
+        coin: 1
+    }, {
+        name: '질리언',
+        synergy: '범죄조직',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 2
+    }, {
+        name: '타릭',
+        synergy: '마법사',
+        synergyIcon: './img/synergy/Socialite.svg',
+        coin: 1
+    }, {
+        name: '하이머딩거',
+        synergy: '요들',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '세라핀',
+        synergy: '범죄조직',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '오리아나',
+        synergy: '범죄조직',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '잔나',
+        synergy: '범죄조직',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '제이스',
+        synergy: '범죄조직',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '다리우스',
+        synergy: '범죄조직',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '자이라',
+        synergy: '범죄조직',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '탈론',
+        synergy: '암살자',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '샤코',
+        synergy: '범죄조직',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '에코',
+        synergy: '범죄조직',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '브라움',
+        synergy: '범죄조직',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '아칼리',
+        synergy: '범죄조직',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '직스',
+        synergy: '범죄조직',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '스웨인',
+        synergy: '마법사',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '말자하',
+        synergy: '마법사',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '벡스',
+        synergy: '마법사',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '럭스',
+        synergy: '마법사',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '빅토르',
+        synergy: '마법사',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    }, {
+        name: '유미',
+        synergy: '고냥이',
+        synergyIcon: './img/synergy/Syndicate_white.svg',
+        coin: 1
+    },
+];
+
+
+
+
 
 
 // 챔프를 배치툴에다 마우스 업을 하면 champ_synergy에 champ_synergy_tab 부분이 생기고 champ_synergy_tab . champData.js에 있는 ChampInformation.name 이랑 같으면 champ_synergy_tab부분 안의 .batch_tool .champ_synergy_tab .synergy_data p 부분을 champData.js에 있는 ChampInformation.synergy를 추가해라
 
-champInformation.forEach(i =>{
-    console.log(champInformation[i].name);
-})
+
