e82e137f8610c9bc753629a12367322fa840de65
KG <rkdwldms9058@naver.com>
2024-04-17 03:47:18 +0900

    챔프를 배치툴에 배치하고 다른 챔프를 마우스 다운 시 배치툴에 있던 챔프가 사라지는 현상, 다른 요소로 챔프를 마우스 드래그 시 기존 요소의 파랑색배경이 계속 남아있는 현상, 시너지 탭의 데이터 값이 첫번째 챔프 요소만 적용되고 나머지 요소는 바뀌지 않는 현상 해결완료

--- script/champData.js
+++ script/champData.js
@@ -2,48 +2,48 @@
 var champInformation = [
     {
         name: '이즈리얼',
-        synergy: '마법사',
-        synergyIcon: './img/synergy/Scholar_white.svg',
+        synergy: '혁신가',
+        synergyIcon: './img/synergy/Innovator_white.svg',
         coin: 1
     }, {
         name: '질리언',
-        synergy: '범죄조직',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '시계태엽',
+        synergyIcon: './img/synergy/Clockwork_white.svg',
         coin: 2
     }, {
         name: '타릭',
-        synergy: '마법사',
-        synergyIcon: './img/synergy/Socialite.svg',
+        synergy: '강화술사',
+        synergyIcon: './img/synergy/Enchanter_white.svg',
         coin: 1
     }, {
         name: '하이머딩거',
         synergy: '요들',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergyIcon: './img/synergy/Cuddly_white.svg',
         coin: 1
     }, {
         name: '세라핀',
-        synergy: '범죄조직',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '사교계',
+        synergyIcon: './img/synergy/Socialite_white.svg',
         coin: 1
     }, {
         name: '오리아나',
-        synergy: '범죄조직',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '시계태엽',
+        synergyIcon: './img/synergy/Clockwork_white.svg',
         coin: 1
     }, {
         name: '잔나',
-        synergy: '범죄조직',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '학자',
+        synergyIcon: './img/synergy/Scholar_white.svg',
         coin: 1
     }, {
         name: '제이스',
-        synergy: '범죄조직',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '혁신가',
+        synergyIcon: './img/synergy/Innovator_white.svg',
         coin: 1
     }, {
         name: '다리우스',
-        synergy: '범죄조직',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '경호대',
+        synergyIcon: './img/synergy/Bodyguard_white.svg',
         coin: 1
     }, {
         name: '자이라',
@@ -53,7 +53,7 @@ var champInformation = [
     }, {
         name: '탈론',
         synergy: '암살자',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergyIcon: './img/synergy/Assassin_white.svg',
         coin: 1
     }, {
         name: '샤코',
@@ -62,53 +62,53 @@ var champInformation = [
         coin: 1
     }, {
         name: '에코',
-        synergy: '범죄조직',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '고물상',
+        synergyIcon: './img/synergy/Scrap_white.svg',
         coin: 1
     }, {
         name: '브라움',
-        synergy: '범죄조직',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '경호대',
+        synergyIcon: './img/synergy/Bodyguard_white.svg',
         coin: 1
     }, {
         name: '아칼리',
-        synergy: '범죄조직',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '암살자',
+        synergyIcon: './img/synergy/Assassin_white.svg',
         coin: 1
     }, {
         name: '직스',
-        synergy: '범죄조직',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '비전마법사',
+        synergyIcon: './img/synergy/Arcanist_white.svg',
         coin: 1
     }, {
         name: '스웨인',
-        synergy: '마법사',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '비전마법사',
+        synergyIcon: './img/synergy/Arcanist_white.svg',
         coin: 1
     }, {
         name: '말자하',
-        synergy: '마법사',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '비전마법사',
+        synergyIcon: './img/synergy/Arcanist_white.svg',
         coin: 1
     }, {
         name: '벡스',
-        synergy: '마법사',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '요들',
+        synergyIcon: './img/synergy/Cuddly_white.svg',
         coin: 1
     }, {
         name: '럭스',
-        synergy: '마법사',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '아카데미',
+        synergyIcon: './img/synergy/Academy_white.svg',
         coin: 1
     }, {
         name: '빅토르',
-        synergy: '마법사',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '비전마법사',
+        synergyIcon: './img/synergy/Arcanist_white.svg',
         coin: 1
     }, {
         name: '유미',
-        synergy: '고냥이',
-        synergyIcon: './img/synergy/Syndicate_white.svg',
+        synergy: '포근',
+        synergyIcon: './img/synergy/Cuddly_white.svg',
         coin: 1
     },
 ];
